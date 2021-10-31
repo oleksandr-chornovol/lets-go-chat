8a3ddd0d60be2483fec8a467a3e4779823c5d020
Олександр Чорновол <oleksandrchornovol@MacBook-Pro-Oleksandr.local>
2021-10-31 23:25:27 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/oleksandr-chornovol/lets-go-chat
+
+go 1.16
+
+require golang.org/x/tools v0.1.7 // indirect
---
+++ pkg/hasher/hasher.go
@@ -0,0 +1,10 @@
+package hasher
+
+func HashPassword(password string) string {
+	return "qwe"
+}
+
+func CheckPasswordHash(password string, hash string) bool {
+	return true
+}
+
