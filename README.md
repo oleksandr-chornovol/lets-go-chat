105ab710cf7d375efda0172040a072169e9f0a3b
Олександр Чорновол <oleksandrchornovol@MacBook-Pro-Oleksandr.local>
2021-11-01 20:45:03 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# lets-go-chat
+Placeholder for the future project (lets-go-chat)
+
+To run the application Go language must be installed (please refer to https://golang.org/doc/install )
+
+The application can be downloaded from GitHub using
+- Web (https://github.com/oleksandr-chornovol/lets-go-chat/archive/refs/heads/master.zip)
+- Git (https://github.com/oleksandr-chornovol/lets-go-chat.git)
+
+To run the application run the following commands:
+<pre>
+    $ cd lets-go-chat
+    $ go run main.go
+</pre>
--- pkg/hasher/hasher.go
+++ pkg/hasher/hasher.go
@@ -1,12 +1,15 @@
+// Package hasher provides functions for hashing string and comparing string with hash
 package hasher
 
 import "golang.org/x/crypto/bcrypt"
 
+// HashPassword returns bcrypt hash of the password
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
 
+// CheckPasswordHash compares password and hash
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
