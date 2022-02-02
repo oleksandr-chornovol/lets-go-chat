2c35837469c4816f6128cc9e0655a9da61390c73
Олександр Чорновол <oleksandrchornovol@MacBook-Pro-Oleksandr.local>
2022-02-02 14:06:04 +0200

    added pprof

--- app/http/router.go
+++ app/http/router.go
@@ -3,7 +3,9 @@ package http
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"log"
 	"net/http"
+	"net/http/pprof"
 )
 
 var router = chi.NewRouter()
@@ -24,4 +26,18 @@ func InitRoutes() {
 	router.Post("/v1/user/login", userController.Login)
 	router.Get("/v1/user/active", chatController.GetActiveUsersCount)
 	router.Get("/v1/chat", chatController.StartChat)
+
+	initPprofRoutes()
+}
+
+func initPprofRoutes()  {
+	router.Get("/debug/pprof/", pprof.Index)
+	router.Get("/debug/pprof/cmdline", pprof.Cmdline)
+	router.Get("/debug/pprof/profile", pprof.Profile)
+	router.Get("/debug/pprof/symbol", pprof.Symbol)
+	router.Get("/debug/pprof/trace", pprof.Trace)
+
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
 }
